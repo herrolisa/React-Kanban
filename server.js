a83caa7269e812d88bca2732717a6ca866018395
Lisa <misslisazhou@gmail.com>
2016-08-16 23:35:59 -1000

    set up pug to render index page

--- server.js
+++ server.js
@@ -9,14 +9,26 @@ const db = require('./models');
 const express = require('express');
 const app = express();
 
+// SET UP PUG TEMPLATES
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, 'views'));
+
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//CORS
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+// ROUTES ======================================================================
 app.get('/', function (req, res) {
-  res.send('You are online!');
+  res.render('index');
 });
 
 app.listen(app.get('port'), function () {
