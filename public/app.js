cca473d82a099aa4a3bf2af001e5424895b5d4ac
Lisa <misslisazhou@gmail.com>
2016-08-17 20:55:10 -1000

    start all over with one column (will add other two later)

--- public/app.js
+++ public/app.js
@@ -1,39 +1,66 @@
-const CardForm = React.createClass({
+// -KanbanBox
+//   -Column
+//     -Card
+//     -CardForm
+
+const TaskForm = React.createClass({
   render: function () {
     return (
-      <form className="cardForm" onSubmit={this.handleSubmit} >
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Priority" />
-        <input type="text" placeholder="Created By" />
-        <input type="text" placeholder="Assigned To" />
+      <form className="taskForm" onSubmit={this.handleSubmit} >
+        <input type="text" placeholder="Title" /><br />
+        <input type="text" placeholder="Priority" /><br />
+        <input type="text" placeholder="Created By" /><br />
+        <input type="text" placeholder="Assigned To" /><br />
         <input type="submit" value="Add New Task" />
       </form>
     );
   }
 });
 
-const Card = React.createClass({
+const TaskCard = React.createClass({
   render: function() {
     return (
       <div className="task-card">
         <ul>
-          <li>Title:</li>
-          <li>Priority:</li>
-          <li>Created By:</li>
-          <li>Assigned To:</li>
+          <li>Title: {this.props.title}</li>
+          <li>Priority: {this.props.priority}</li>
+          <li>Created By: {this.props.created_by}</li>
+          <li>Assigned To: {this.props.assigned_to}</li>
         </ul>
       </div>
     );
   }
 });
 
+const TaskList = React.createClass({
+  render: function () {
+    var tasks = this.props.data.map(function(singleTask) {
+      return (
+        <TaskCard
+          title={singleTask.title}
+          priority={singleTask.priority}
+          created_by={singleTask.created_by}
+          assigned_to={singleTask.assigned_to}
+          key={singleTask.id}
+        />
+      );
+    });
+    return (
+      <div className="task-list">
+        { tasks }
+      </div>
+    );
+  }
+});
+
+
 const Column = React.createClass({
   render: function() {
     return (
       <div className="column">
-        <h2>{this.props.status}</h2>
-        <Card />
-        <CardForm />
+        <h2>Things To Do</h2>
+        <TaskList data={this.props.data} />
+        <TaskForm />
       </div>
     );
   }
@@ -41,23 +68,20 @@ const Column = React.createClass({
 
 const KanbanBox = React.createClass({
  render: function () {
-    const statusColumns = this.props.data.map(function (element, index) {
-        return (
-          <Column key={index} status={element.status} />
-        )
-    })
     return (
       <div className='kanban-container'>
         <h1>Kanban Box</h1>
-        {statusColumns}
+        <Column data={this.props.data} />
       </div>
     );
   }
 });
 
-var statusArr = [{status: 'Queue'}, {status: 'In Progress'}, {status: 'Done'}]
+var sample = [
+  {"id":2,"title":"Sales Report","priority":50,"created_by":"Manager","assigned_to":"Intern","createdAt":"2016-08-17T23:47:29.372Z","updatedAt":"2016-08-17T23:47:29.372Z"},
+  {"id":3,"title":"Expense Report","priority":50,"created_by":"Manager","assigned_to":"Intern","createdAt":"2016-08-17T23:47:29.372Z","updatedAt":"2016-08-17T23:47:29.372Z"}];
 
 ReactDOM.render(
-  <KanbanBox data={statusArr} />,
+  <KanbanBox data={sample} />,
   document.getElementById('container')
 );
