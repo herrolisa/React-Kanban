e5fd8db37dab823b603167a8c27d9416d5d0ec0f
Lisa <misslisazhou@gmail.com>
2016-08-18 21:55:29 -1000

    update the seeders with more tasks

--- seeders/20160817074953-add-users.js
+++ seeders/20160817074953-add-users.js
@@ -3,17 +3,32 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      username : 'Manager',
+      username : 'Executive Director',
       createdAt : new Date(),
       updatedAt : new Date(),
     },
     {
-      username : 'Director',
+      username : 'Sales Manager',
       createdAt : new Date(),
       updatedAt : new Date(),
     },
     {
-      username : 'Intern',
+      username : 'Human Resource Director',
+      createdAt : new Date(),
+      updatedAt : new Date(),
+    },
+    {
+      username : 'Accountant',
+      createdAt : new Date(),
+      updatedAt : new Date(),
+    },
+    {
+      username : 'Office Manager',
+      createdAt : new Date(),
+      updatedAt : new Date(),
+    },
+    {
+      username : 'Student Intern',
       createdAt : new Date(),
       updatedAt : new Date(),
     }], {});
--- seeders/20160817083746-add-tasks.js
+++ seeders/20160817083746-add-tasks.js
@@ -4,17 +4,41 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Tasks', [{
       title : 'Sales Report',
-      priority: 50,
-      created_by: 'Manager',
-      assigned_to: 'Intern',
+      priority: 22,
+      created_by: 'Sales Manager',
+      assigned_to: 'Student Intern',
       createdAt : new Date(),
       updatedAt : new Date()
     },
     {
       title : 'Expense Report',
-      priority: 50,
-      created_by: 'Manager',
-      assigned_to: 'Intern',
+      priority: 47,
+      created_by: 'Executive Director',
+      assigned_to: 'Accountant',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    },
+    {
+      title : 'Buy Office Supplies',
+      priority: 85,
+      created_by: 'Executive Director',
+      assigned_to: 'Office Manager',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    },
+    {
+      title : 'Plan New Employee Orientation',
+      priority: 58,
+      created_by: 'Human Resource Director',
+      assigned_to: 'Human Resource Director',
+      createdAt : new Date(),
+      updatedAt : new Date()
+    },
+    {
+      title : 'Make Everyone a Sandwich',
+      priority: 58,
+      created_by: 'Executive Director',
+      assigned_to: 'Student Intern',
       createdAt : new Date(),
       updatedAt : new Date()
     }], {});
