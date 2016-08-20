bf99d4d66ffc16cdc98a40c5519b6fb38de011a6
Lisa <misslisazhou@gmail.com>
2016-08-20 01:08:54 -1000

    add status_id to Tasks table

---
+++ migrations/20160820083205-add-statusID-to-tasks.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface
+      .addColumn('Tasks', 'status_id', {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        foreignKey: true,
+        references: {
+          model: 'Statuses',
+          key: 'id'}
+      });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface
+      .removeColumn('Tasks', 'status_id');
+  }
+};
