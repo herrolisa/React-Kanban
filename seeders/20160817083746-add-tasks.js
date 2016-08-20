cc58a29491a0056f1f8fd2a9f1cba0f072c3aa72
Lisa <misslisazhou@gmail.com>
2016-08-20 01:09:49 -1000

    add status_id to Tasks table

--- seeders/20160817083746-add-tasks.js
+++ seeders/20160817083746-add-tasks.js
@@ -4,41 +4,46 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Tasks', [{
       title : 'Sales Report',
-      priority: 22,
+      priority: 2,
       created_by: 'Sales Manager',
       assigned_to: 'Student Intern',
+      status_id: 1,
       createdAt : new Date(),
       updatedAt : new Date()
     },
     {
       title : 'Expense Report',
-      priority: 47,
+      priority: 4,
       created_by: 'Executive Director',
       assigned_to: 'Accountant',
+      status_id: 1,
       createdAt : new Date(),
       updatedAt : new Date()
     },
     {
       title : 'Buy Office Supplies',
-      priority: 85,
+      priority: 5,
       created_by: 'Executive Director',
       assigned_to: 'Office Manager',
+      status_id: 2,
       createdAt : new Date(),
       updatedAt : new Date()
     },
     {
       title : 'Plan New Employee Orientation',
-      priority: 58,
+      priority: 8,
       created_by: 'Human Resource Director',
       assigned_to: 'Human Resource Director',
+      status_id: 1,
       createdAt : new Date(),
       updatedAt : new Date()
     },
     {
-      title : 'Make Everyone a Sandwich',
-      priority: 58,
+      title : 'Make Me A Sandwich!',
+      priority: 10,
       created_by: 'Executive Director',
       assigned_to: 'Student Intern',
+      status_id: 3,
       createdAt : new Date(),
       updatedAt : new Date()
     }], {});
