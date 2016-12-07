7c4ad05f4cf435c1b19ca035ed510ddd3ec27fe7
Lisa <misslisazhou@gmail.com>
2016-12-06 21:32:38 -1000

    edit seeders file for tasks

--- seeders/20160817083746-add-tasks.js
+++ seeders/20160817083746-add-tasks.js
@@ -3,15 +3,6 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Tasks', [{
-      title : 'Sales Report',
-      priority: 2,
-      created_by: 'Sales Manager',
-      assigned_to: 'Student Intern',
-      status_id: 1,
-      createdAt : new Date(),
-      updatedAt : new Date()
-    },
-    {
       title : 'Expense Report',
       priority: 7,
       created_by: 'Executive Director',
@@ -34,15 +25,6 @@ module.exports = {
       priority: 4,
       created_by: 'Human Resource Director',
       assigned_to: 'Human Resource Director',
-      status_id: 1,
-      createdAt : new Date(),
-      updatedAt : new Date()
-    },
-    {
-      title : 'Make Me A Sandwich!',
-      priority: 10,
-      created_by: 'Executive Director',
-      assigned_to: 'Student Intern',
       status_id: 3,
       createdAt : new Date(),
       updatedAt : new Date()
